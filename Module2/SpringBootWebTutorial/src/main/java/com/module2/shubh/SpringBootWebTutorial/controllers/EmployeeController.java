690e8ef6447e494b5537a5d9d3760eb601f404e7
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-01 20:00:06 +0530

    M2 - DTO,Path_Params in Get Request

--- Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.module2.shubh.SpringBootWebTutorial.controllers;
 
+import com.module2.shubh.SpringBootWebTutorial.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 // The annotation below makes sure that mappings defined are actually REST in nature
 // Rest Controller uses @Controller & @ResponseBody annotations behind the scenes
@@ -9,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 // directly to the response body.
 @RestController
 public class EmployeeController {
-//    Component Scan ensures that whenever this resource endpoint is called with a get request
-//    then the appropriate controller method is run to respond back to the client
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage(){
-        return "Secret Message:fshdjghs@sejnh";
+
+    // Defining a path parameter in resource endpoint
+    // Marking the argument in method using @PathVariable annotation that will use the path param's value
+    // Note(IMP): Always make sure that path param and function argument are exactly the same
+    // Otherwise the method won't know which param to use.
+    @GetMapping(path = "/employees/{employeeId}")
+    public EmployeeDTO getEmployeeByID(@PathVariable long employeeId) {
+        return new EmployeeDTO(employeeId,"Shubh","shubh@gaur.com",26, LocalDate.now(),true);
     }
 }
 
---
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/dto/EmployeeDTO.java
@@ -0,0 +1,73 @@
+package com.module2.shubh.SpringBootWebTutorial.dto;
+
+import java.time.LocalDate;
+// POJO: Plain Old Java Object
+public class EmployeeDTO {
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+
+    public EmployeeDTO() {
+        this.id = id;
+    }
+
+    public EmployeeDTO(long id, String name, String email, int age, LocalDate dateOfJoining, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfJoining = dateOfJoining;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
