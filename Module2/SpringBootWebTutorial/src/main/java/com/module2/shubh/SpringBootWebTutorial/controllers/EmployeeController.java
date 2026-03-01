4cc59b255ab9c399196dc15e5c608d2625e7cd0c
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-01 20:19:57 +0530

    M2 - @RequestMapping defines parent resource path on the controller.

--- Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
@@ -1,10 +1,7 @@
 package com.module2.shubh.SpringBootWebTutorial.controllers;
 
 import com.module2.shubh.SpringBootWebTutorial.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -13,23 +10,19 @@ import java.time.LocalDate;
 // making sure all methods defined within the controller automatically return JSON/XML responses
 // directly to the response body.
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
-
-    // Defining a path parameter in resource endpoint
-    // Marking the argument in method using @PathVariable annotation that will use the path param's value
-    // Note(IMP): Always make sure that path param and function argument are exactly the same
-    // Otherwise the method won't know which param to use.
-    @GetMapping(path = "/employees/{employeeId}")
+//    Having to define the endpoint in every type of get request for this resource is extra work
+//    Instead we can define a top level resource endpoint on the controller that makes sure
+//    all requests for the employee resource are routed to appropriate methods within this controller
+//    it also enables us to eliminate redundant definition of employee resource on the get mappings
+//    In essence, /employees acts as a prefix for all the defined mappings
+    @GetMapping(path = "/{employeeId}")
     public EmployeeDTO getEmployeeByID(@PathVariable long employeeId) {
         return new EmployeeDTO(employeeId,"Shubh","shubh@gaur.com",26, LocalDate.now(),true);
     }
 
-//    Defining Query Parameters
-//    making them optional requires setting the required field to false
-//    Otherwise, the request will only work when all required fields are included in the query params
-//    One more thing, is that we needed to append a "/" after the resource to allow adding query params
-//    in the endpoint, simply a "/employees" mapping won't work to inject the query params.
-    @GetMapping(path = "/employees/")
+    @GetMapping(path = "/")
     public String getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
         return "Hi, Age = " + age + " | "  + sortBy;
     }
