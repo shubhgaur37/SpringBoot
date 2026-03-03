ed19996bcdcca74630889aebbe577322b25d5b7a
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-03 15:29:51 +0530

    Hibernate - Entity to Table

---
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/entities/EmployeeEntity.java
@@ -0,0 +1,29 @@
+package com.module2.shubh.SpringBootWebTutorial.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity // This notation tells hibernate that we need a table of this class
+// @Table : used for defining the name of table, table would be created even if this annotation is
+// not specified but with class name, @Table gives us flexibility to add properties like indexes, constraints
+@Table(name = "employees")
+public class EmployeeEntity {
+    @Id // used to specify primary key for the table
+    // used to enforce strategy to ensure uniqueness eg. auto-increment, sequence,
+    // here it chooses generation strategy automatically
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+}
+
+// this is not code duplication as it might look comparing EmployeeDTO
+// In case we want to store sensitive information here and avoid exposing it on the dto side.
+// DTO is used to specify request response validation logic, while this class represents the constraints,
+// schema, properties that these fields must adhere to in the DB
+
