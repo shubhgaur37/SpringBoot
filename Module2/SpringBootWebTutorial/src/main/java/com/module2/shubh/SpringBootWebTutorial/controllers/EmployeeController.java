7dfc7e829b708f05dc68e15ac96236622c41731a
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-04 19:15:52 +0530

    Introducing Service Layer b/w Presentation and Persistence Layers

--- Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/controllers/EmployeeController.java
@@ -1,8 +1,9 @@
 package com.module2.shubh.SpringBootWebTutorial.controllers;
 
 import com.module2.shubh.SpringBootWebTutorial.entities.EmployeeEntity;
-import com.module2.shubh.SpringBootWebTutorial.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
+import com.module2.shubh.SpringBootWebTutorial.service.EmployeeService;
+
 import java.util.List;
 
 // The annotation below makes sure that mappings defined are actually REST in nature
@@ -12,41 +13,37 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
-//    bad practice for controller to directly communicate with
-//    persistence layer[DB], best practice is to use a service layer in between
-//    for demonstration purposes
-    EmployeeRepository employeeRepository;
 
-    EmployeeController(EmployeeRepository employeeRepository) {
-//        will be automatically injected as Employee Repository internally is a bean
-        this.employeeRepository = employeeRepository;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+//        will be automatically injected as Employee Service internally is a bean
+        this.employeeService = employeeService;
     }
 
     @GetMapping(path = "/{employeeId}")
-//    bad practice: revisited
     public EmployeeEntity getEmployeeByID(@PathVariable(name = "employeeId") Long id) {
-//        returns an optional object, which helps us deal with null pointer exceptions
-//        in a graceful way using methods specifying the exact steps to take
-//        to deal with such exceptions
-        return employeeRepository.findById(id).orElse(null);
+        return employeeService.findById(id);
     }
 
     @GetMapping
     public List<EmployeeEntity> getAllEmployees() {
-        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
 //    Request Body makes sure to map the request correctly with the dto after matching
 //    the fields passed in the request.
     @PostMapping
     public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee) {
-        return employeeRepository.save(inputEmployee);
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping
     public String updateEmployeeById(){
         return "HELLO from PUT";
     }
-
 }
 
+// We were able to get rid of the Employee Repository by introducing a Service layer in between to interact with database(repository) which
+// a good practice as we don't want direct interaction of db with clients and only allow service to communicate with it.
+// In this way, we can abstract out complex validation, intermediate steps from the presentation or client facing layer
