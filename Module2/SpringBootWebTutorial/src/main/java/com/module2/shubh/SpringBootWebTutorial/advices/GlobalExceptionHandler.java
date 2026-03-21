dcf23a4a46feb5a1943d98ca43e0932e35702c4d
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-21 16:07:43 +0530

    Adding Validation Exception Handler and a generic handler

--- Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/advices/GlobalExceptionHandler.java
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/advices/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ package com.module2.shubh.SpringBootWebTutorial.advices;
 import com.module2.shubh.SpringBootWebTutorial.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 // annotation used to define a global exception handler
 // applies globally to all controllers
@@ -19,7 +22,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception){
 //        getting exception from the message
-//        useful when we want to throw same expression with different error at multiple points within the application
+//        useful when we want to throw exceptions with custom error messages at multiple points within the application
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
@@ -27,4 +30,34 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
 //        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
     }
+
+    // Handle all other exceptions except Resource Not Found
+    // in case of Resource not found the control will go to first handler as it is more granular
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleInternalServerError(Exception exception){
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Exception Handler for invalid inputs
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception){
+        // get list of all the errors based on the error messages defined on the validations
+        // getBinding Results
+        List<String> errors = exception.getBindingResult().getAllErrors()
+                .stream()
+                .map(error ->error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                // convert list of errors into message for good debugging
+                .message(errors.toString())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
