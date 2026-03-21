68300e6ac2206d375d37a6ab2c640ac133871483
shubhgaur37 <shubhgaur7833@gmail.com>
2026-03-21 15:49:50 +0530

    adding null check in role validator

--- Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/annotations/EmployeeRoleValidator.java
+++ Module2/SpringBootWebTutorial/src/main/java/com/module2/shubh/SpringBootWebTutorial/annotations/EmployeeRoleValidator.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class EmployeeRoleValidator implements ConstraintValidator<EmployeeRoleValidation, String> {
     @Override
     public boolean isValid(String inputRole, ConstraintValidatorContext constraintValidatorContext) {
+        if (inputRole == null) return false;
         List<String> validRoles = List.of("ADMIN","USER");
         return validRoles.contains(inputRole);
     }
