6ee9dd625079e9b1eef102e227394c0d1d331f61
shubhgaur37 <shubhgaur7833@gmail.com>
2026-06-23 23:51:00 +0530

    Add Entity Level Auditing using EntityListeners

--- Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/config/AppConfig.java
+++ Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.shubh.module4.Prod_Ready_Features.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class AppConfig {
 
     @Bean
--- Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/entity/PostEntity.java
+++ Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/entity/PostEntity.java
@@ -3,14 +3,22 @@ package com.shubh.module4.Prod_Ready_Features.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Entity
-@Table(name = "posts")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "posts")
+@EntityListeners(AuditingEntityListener.class) // Allows some annotations for auditing, populates the fields automatically
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +27,19 @@ public class PostEntity {
     String title;
 
     String description;
+
+    // Entity Listener Annotations
+    @CreatedDate
+    LocalDateTime createdDate;
+
+    @LastModifiedDate
+    LocalDateTime updatedDate;
+
+    @CreatedBy
+    String createdBy;
+
+    @LastModifiedBy
+    String updatedBy;
+
 
 }
