ae6687289fcaceb1b7b44016517002981a7709b9
shubhgaur37 <shubhgaur7833@gmail.com>
2026-06-24 22:48:40 +0530

    Global Handling of common error: Internal Server Error

--- Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/config/RestClientConfig.java
+++ Prod_Ready_Features/src/main/java/com/shubh/module4/Prod_Ready_Features/config/RestClientConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
@@ -38,6 +39,9 @@ public class RestClientConfig {
         return RestClient.builder()
                 .baseUrl(BASE_URL)
                 .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .defaultStatusHandler(HttpStatusCode::is5xxServerError, (req,res) -> {
+                    throw new RuntimeException("Server Error Occurred");
+                }) // default handling to avoid handling server error status everywhere
                 .build();
     }
 }
