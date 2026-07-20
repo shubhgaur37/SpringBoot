5eebaaa1ffac689b7a9dbaf6aa536271bd0f742f
shubhgaur37 <shubhgaur7833@gmail.com>
2026-07-20 22:56:50 +0530

    Adding TestContainer Configuration for mysql

---
+++ M7_TestingMethodologies/src/test/java/com/Shubh/Module7/M7_TestingMethodologies/TestContainersConfiguration.java
@@ -0,0 +1,53 @@
+package com.Shubh.Module7.M7_TestingMethodologies;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+// Defines additional beans that are loaded only for tests.
+// Unlike @Configuration, this class is not automatically detected during
+// component scanning and must be explicitly imported into test classes.
+@TestConfiguration
+public class TestContainersConfiguration {
+
+    // In-memory databases (such as H2) are fast and convenient for testing,
+    // but they may not fully replicate the behavior of the production
+    // database. Running tests against a real MySQL instance provides greater
+    // confidence by validating SQL dialects, transactions, constraints,
+    // indexing, and other database-specific features.
+    //
+    // Testcontainers automatically:
+    //   • Pulls the Docker image if it is not available locally.
+    //   • Starts the container before the tests execute.
+    //   • Stops and removes the container after the tests complete.
+    @Bean
+
+    // Marks this Testcontainer as the application's database connection.
+    // Spring Boot automatically configures the DataSource using the running
+    // container's JDBC URL, username, password, and driver.
+    @ServiceConnection
+    MySQLContainer<?> mySQLContainer() {
+
+        // MySQLContainer is a generic class. The <?> wildcard indicates that we
+        // don't need to specify its exact generic type here.
+        // MySQLContainer uses generics internally to support
+        // fluent method chaining, but we do not need to reference the type.
+        return new MySQLContainer<>(
+                DockerImageName.parse("mysql:5.7.34") // docker image name from docker repository
+        )
+                // Creates a database named 'employee_db' inside the container.
+                .withDatabaseName("employee_db")
+
+                // Creates the default database user.
+                .withUsername("test_user")
+                .withPassword("test_password")
+
+                // Assigns a fixed Docker container name.
+                // Useful for debugging, but random names are generally
+                // preferred in automated test suites to avoid conflicts.
+                .withCreateContainerCmdModifier(cmd ->
+                        cmd.withName("employee-mysql-test"));
+    }
+}
