f6d1e6dd51b25efe9c26ae3e577f49f39e294210
shubhgaur37 <shubhgaur7833@gmail.com>
2026-07-21 19:47:46 +0530

    reformat

--- M7_TestingMethodologies/src/main/java/com/Shubh/Module7/M7_TestingMethodologies/exception/GlobalExceptionHandler.java
+++ M7_TestingMethodologies/src/main/java/com/Shubh/Module7/M7_TestingMethodologies/exception/GlobalExceptionHandler.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Void> handleResourceNotFoundException(ResourceNotFoundException ex){
+    public ResponseEntity<Void> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<Void> handleDuplicateResourceException(DuplicateResourceException ex){
+    public ResponseEntity<Void> handleDuplicateResourceException(DuplicateResourceException ex) {
         return ResponseEntity.badRequest().build();
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Void> handleRuntimeException(RuntimeException ex){
+    public ResponseEntity<Void> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.internalServerError().build();
     }
 }
