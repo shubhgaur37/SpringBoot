6ccfc62fc72477e4575dc8a804a109b62e192371
shubhgaur37 <shubhgaur7833@gmail.com>
2026-07-21 19:47:31 +0530

    Added integration tests for all flows of /employees endpoint

--- M7_TestingMethodologies/src/test/java/com/Shubh/Module7/M7_TestingMethodologies/controller/EmployeeControllerTestIT.java
+++ M7_TestingMethodologies/src/test/java/com/Shubh/Module7/M7_TestingMethodologies/controller/EmployeeControllerTestIT.java
@@ -145,12 +145,127 @@ class EmployeeControllerTestIT {
                 .expectStatus().isNotFound();
     }
 
+    @Test
+    void testCreateNewEmployee_success() {
+
+        // ---------------------- Arrange ----------------------
+        // No database setup is required. The employee is created by sending an
+        // HTTP POST request to the controller.
+
+        // -------------------- Act & Assert --------------------
+
+        webTestClient.post()
+
+                // Target the employee creation endpoint.
+                .uri("/employees")
+
+                // Serialize(Memory Object to JSON) the EmployeeDTO into JSON and send it as the request
+                // body. Spring automatically deserializes it back into an
+                // EmployeeDTO parameter annotated with @RequestBody.
+                .bodyValue(testEmployeeDTO)
+
+                // Send the HTTP request.
+                .exchange()
+
+                // Verify that a new employee was created successfully.
+                .expectStatus().isCreated()
+
+                // Verify selected fields in the JSON response using JSONPath.
+                // JSONPath provides a convenient way to assert individual JSON
+                // properties without deserializing the entire response into an
+                // EmployeeDTO object.
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(testEmployeeDTO.getId())
+                .jsonPath("$.email").isEqualTo(testEmployeeDTO.getEmail());
+    }
+
+    @Test
+    void testCreateNewEmployee_failure() {
+        employeeRepository.save(testEmployee);
+
+        webTestClient.post()
+                .uri("/employees")
+                .bodyValue(testEmployeeDTO)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+
+    @Test
+    void testUpdateEmployeeByID_success() {
+        employeeRepository.save(testEmployee);
+        Long id = testEmployeeDTO.getId();
+
+        webTestClient.put()
+                .uri("/employees/{id}", id)
+                .bodyValue(testEmployeeDTO)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(EmployeeDTO.class)
+                .value(fetchedEmployeeDTO ->
+                        assertThat(fetchedEmployeeDTO).isEqualTo(testEmployeeDTO));
+    }
+
+    @Test
+    void testUpdateEmployeeByID_failureEmployeeNotfound() {
+        Long id = testEmployeeDTO.getId();
+
+        webTestClient.put()
+                .uri("/employees/{id}", id)
+                .bodyValue(testEmployeeDTO)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testUpdateEmployeeByID_failureEmailModification_throwsException() {
+        // save the employee
+        employeeRepository.save(testEmployee);
+
+        Long id = testEmployeeDTO.getId();
+
+        // tamper the email
+        testEmployeeDTO.setEmail("abc@xyz.com");
+
+        webTestClient.put()
+                .uri("/employees/{id}", id)
+                .bodyValue(testEmployeeDTO)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+
+    @Test
+    void testDeleteEmployeeByID_success() {
+        // save the employee
+        employeeRepository.save(testEmployee);
+
+        Long id = testEmployeeDTO.getId();
+
+        webTestClient.delete()
+                .uri("/employees/{id}", id)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+
+    @Test
+    void testDeleteEmployeeByID_failureNotFound() {
+        Long id = testEmployeeDTO.getId();
+
+        webTestClient.delete()
+                .uri("/employees/{id}", id)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+
     // JUnit executes test methods sequentially by default, so this cleanup runs
     // only after the current test has finished. Removing all records ensures the
     // next test starts with a clean database and remains independent of the
     // execution order.
     @AfterEach()
-    void tearDown(){
+    void tearDown() {
         employeeRepository.deleteAll();
     }
 }
